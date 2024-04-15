54b5bf3879120b912376c1d1bf076c5ada42f0c6
Wangdi <wangdi7670@163.com>
2024-04-15 15:04:10 +0000

    done program1

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -36,6 +36,20 @@ void workerThreadStart(WorkerArgs * const args) {
     // half of the image and thread 1 could compute the bottom half.
 
     printf("Hello world from thread %d\n", args->threadId);
+
+    int delta = args->height / args->numThreads;
+    int startRow = args->threadId * delta;
+    int totalRows = delta;
+
+    if (args->threadId == args->numThreads - 1) {
+        totalRows = args->height - startRow;
+    }
+
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, 
+        args->width, args->height,
+        startRow, totalRows,
+        args->maxIterations,
+        args->output);
 }
 
 //
